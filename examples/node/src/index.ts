3872d96550b4bfb24a3b6f2776e68a6a5844d006
Chandler Roth <hey@chandlerroth.com>
2024-06-26 13:50:34 -0400

    Initial SDK setup (#1)

--- examples/node/src/index.ts
+++ examples/node/src/index.ts
@@ -1,20 +1,18 @@
-import "./pre-start"; // Must be the first import
-import logger from "jet-logger";
+import './pre-start'; // Must be the first import
+import logger from 'jet-logger';
 
-import EnvVars from "@src/common/EnvVars";
-import server from "./server";
+import EnvVars from '@src/common/EnvVars';
+import server from './server';
 
-import { init } from "@alerty/sdk";
+import * as Alerty from '@alerty/node';
 
-init({
-  serviceName: "alerty-example-node",
-  serviceVersion: "dev",
-  deploymentEnvironment: "local",
+Alerty.configure({
+  dsn: process.env.ALERTY_DSN!,
 });
 
 // **** Run **** //
 
 const SERVER_START_MSG =
-  "Express server started on port: " + EnvVars.Port.toString();
+  'Express server started on port: ' + EnvVars.Port.toString();
 
 server.listen(EnvVars.Port, () => logger.info(SERVER_START_MSG));
